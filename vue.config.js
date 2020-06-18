fc4cfa804577815eed23c6a2623c7f9576018f10
xuzhou123 <895118827@qq.com>
2020-06-18 23:44:08 +0800

    修改title和ico

--- vue.config.js
+++ vue.config.js
@@ -33,5 +33,24 @@ module.exports = {
             'axios': 'axios',
             'element-ui': 'ELEMENT'
         }
-    }
+    },
+    // 构建多页面应用，页面的配置
+    pages: {
+        index: {
+            // page 的入口
+            entry: 'src/main.js',
+            // 模板来源
+            template: 'public/index.html',
+            // 在 dist/index.html 的输出
+            filename: 'index.html',
+            // 当使用 title 选项时，
+            // template 中的 title 标签需要是 <title><%= htmlWebpackPlugin.options.title %></title>
+            title: '海螺视频',
+            // 在这个页面中包含的块，默认情况下会包含
+            // 提取出来的通用 chunk 和 vendor chunk。
+            chunks: ['chunk-vendors', 'chunk-common', 'index'],
+            // 设置ico
+            favicon: './public/favicon1.ico',
+        }
+    },
 }
